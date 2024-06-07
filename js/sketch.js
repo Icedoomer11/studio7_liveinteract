7bba5e116c0cce2473edee95338d478ef34f1c3b
Icedoomer11 <156988725+Icedoomer11@users.noreply.github.com>
2024-06-07 13:54:24 +0500

    firstpage

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+	createCanvas(200,200);
+	background(220);
+	ellipse(50,50,80,80);
+}
+
+function draw() {
+  // put drawing code here
+  circle(100,100,100);
+}
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+	createCanvas(200,200);
+	background(220);
+	ellipse(50,50,80,80);
+}
+
+function draw() {
+	
+}
